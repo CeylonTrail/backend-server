4b320d2c34e0d8e2d0ed3bf406cd66c140997d51
rlakmal <rruchiralakmal@gmail.com>
2024-07-31 23:54:12 +0530

    retrive a trip

--- src/main/java/com/ceylontrail/backend_server/repo/EventRepo.java
+++ src/main/java/com/ceylontrail/backend_server/repo/EventRepo.java
@@ -3,6 +3,10 @@ package com.ceylontrail.backend_server.repo;
 import com.ceylontrail.backend_server.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepo extends JpaRepository<EventEntity,Integer> {
 
+
+    List<EventEntity> findAllByTrip_TripId(int tripId);
 }
